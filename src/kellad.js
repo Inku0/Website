b3343ca4058345e751cca6fc6ca5a6217ea950d8
Inku0 <46846071+Inku0@users.noreply.github.com>
2021-05-03 14:00:13 +0300

    muutsin nime korrektseks ja eemaldasin manuaalsuse

--- src/kellad.js
+++ src/kellad.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import Countdown from "react-countdown";
 
+function getDate(year, day){
+  	var date = new Date(year, 0);
+  	return new Date(date.setDate(day));
+	}
+
 class Clock extends React.Component {
+	
   constructor(props) {
     super(props);
     this.state = {
       date: new Date(),
-      koolLopp: new Date(1591416900000),
-      koolAlgus: new Date(1598933700000)
+      koolLopp: getDate(new Date().getFullYear(), 158),
+      koolAlgus: getDate(new Date().getFullYear(), 244)
     };
   }
 
@@ -29,20 +35,20 @@ class Clock extends React.Component {
     return (
       <div className='kellad'>
         <div>
-        <h4 className='kell-p'>Praegu</h4>
-        <h2>
-        <span>
+        	<h4 className='kell-p'>Praegu</h4>
+        	<h2>
+        	<span>
           {this.state.date.toLocaleTimeString('en-GB')}
-        </span>
-        </h2>
-        <h4 className='kell-p'>Kool lõppeb</h4>
-        <h2>
-          <Countdown date = {this.state.koolLopp} />
-        </h2>
-        <h4 className='kell-p'>Kool algab</h4>
-        <h2>
-        <Countdown date = {this.state.koolAlgus} />
-        </h2>
+        	</span>
+        	</h2>
+        	<h4 className='kell-p'>Kool lõppeb</h4>
+        	<h2>
+          	<Countdown date = {this.state.koolLopp} />
+        	</h2>
+        	<h4 className='kell-p'>Kool algab</h4>
+        	<h2>
+        		<Countdown date = {this.state.koolAlgus} />
+        	</h2>
         </div>
       </div>
     );
