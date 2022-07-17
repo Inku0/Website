1d83e95487e370dcbaade012dbdbccdea971ce5d
inku <ingvar@saarend.com>
2022-07-17 23:11:01 +0300

    added tailwindcss functionality (autocomplete)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `ingvarruulib`,
-    siteUrl: `https://www.ingvarruulib.com`
+    siteUrl: `https://www.yourdomain.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
--- src/pages/kellad.tsx
+++ src/pages/kellad.tsx
@@ -1,13 +1,29 @@
 import * as React from "react";
-import styled from "styled-components";
+import Countdown from "react-countdown";
 
 const IndexPage = () => {
+    const [date, setDate] = React.useState(new Date());
+    React.useEffect(() => {
+        const timerID = setInterval( () => tick(), 1000 );
+        return function cleanup() {
+            clearInterval(timerID);
+        };
+    });
+
+    const tick = () => {
+        setDate(new Date());
+    };
+    const currentYear = new Date().getFullYear();
     return (
-        <Main>
-            <title>Home Page</title>
-            <BigTitle>Millist tööriista soovite kasutada?</BigTitle>
-            <StyledA href={"kellad.tsx"}>Kellad</StyledA>
-        </Main>
+        <main className={"w-screen h-screen bg-neutral-900 "}>
+            <title>Kellad</title>
+            <h1 className="text-5xl font-bold text-emerald-500 text-center pt-5">Kellad</h1>
+            <span className={"inline-block border rounded-sm text-slate-100"}>{new Date().toLocaleTimeString('en-GB')}</span>
+            <br />
+            <Countdown className={"inline-block border-2 p-2 w-28 rounded-sm text-slate-100"} date={`${currentYear}-09-01T12:00:00`} />
+            <br />
+            <Countdown className={"inline-block border rounded-sm text-slate-100 "} date={`${currentYear + 1}-06-06T12:00:00`} />
+        </main>
     )
 }
 
