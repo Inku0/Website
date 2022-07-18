a26b24e0c9bdf2c5cca3d7e3dfd74ae4d556df5b
inku <ingvar@saarend.com>
2022-07-18 14:43:40 +0300

    index buttons were too big on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ const IndexPage = () => {
       <title>Ingvar Ruulib!</title>
       <h1 className="text-5xl font-bold text-emerald-500 text-center pt-5">Millist tööriista soovite kasutada?</h1>
       <div className={"justify-evenly flex text-center"}>
-        <a className={"text-slate-100 mt-60 rounded-lg w-96 text-center leading-6 bg-neutral-300 text-2xl bg-emerald-500 space-x-4 p-10 "} href={"kellad"}>Kellad</a>
-        <a className={"text-slate-100 mt-60 rounded-lg w-96 text-center leading-6 bg-neutral-300 text-2xl bg-emerald-500 space-x-4 p-10"} href={'https://wolfreealpha.on.fleek.co/input/index.html'}>Wolfree</a>
+        <a className={"text-slate-100 mt-60 rounded w-32 sm:w-96 text-center leading-6 bg-neutral-300 text-2xl bg-emerald-500 space-x-4 p-4 sm:p-10 "} href={"kellad"}>Kellad</a>
+        <a className={"text-slate-100 mt-60 rounded w-32 sm:w-96 text-center leading-6 bg-neutral-300 text-2xl bg-emerald-500 space-x-4 p-4 sm:p-10"} href={'https://wolfreealpha.on.fleek.co/input/index.html'}>Wolfree</a>
       </div>
     </main>
   )
