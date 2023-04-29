765752cf4559be588aad32728754d9ca661c8dbb
inku <ingvar@saarend.com>
2023-04-29 19:47:06 +0300

    removed ky + got fetch() working

--- src/pages/elron.tsx
+++ src/pages/elron.tsx
@@ -1,37 +1,116 @@
 import * as React from "react";
-import ky from 'ky';
+import { TypeAnimation } from 'react-type-animation';
 
-const ElronPage = () => {
-    const getData = async () => {
-        const json = await ky.post('https://example.com', {json: {foo: true}}).json();
-        console.log(json);
-    }
 
-    const Train = () => {
-        getData()
-        return (
+const TrainTimeGetter = () => {
+    const [saueTallinn, setSaueTallinn] = React.useState();
+    const [tallinnSaue, setTallinnSaue] = React.useState();
+    let todayDate = new Date().toISOString().slice(0, 10);
+
+    React.useEffect(() => {
+        const dataFetch = async () => {
+            // waiting for allthethings in parallel
+            const result = (
+                await Promise.all([
+                    fetch(
+                        "https://api.ridango.com/v2/64/intercity/stopareas/trips/direct",
+                        {
+                            method: "PUT",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                "date": todayDate,
+                                "origin_stop_area_id": "64-5847-87",
+                                "destination_stop_area_id": "64-5847-93",
+                                "channel": "web"
+                            })
+                        }
+                    ),
+                    fetch(
+                        "https://api.ridango.com/v2/64/intercity/stopareas/trips/direct",
+                        {
+                            method: "PUT",
+                            headers: {
+                                "Content-Type": "application/json"
+                            },
+                            body: JSON.stringify({
+                                "date": todayDate,
+                                "origin_stop_area_id": "64-5847-93",
+                                "destination_stop_area_id": "64-5847-87",
+                                "channel": "web"
+                            })
+                        }
+                    )
+                ])
+            ).map((r) => r.json());
+
+            // and waiting a bit more - fetch API is cumbersome
+            const [saueTallinn, tallinnSaue] = await Promise.all(
+                result
+            );
+
+            // when the data is ready, save it to state
+            setSaueTallinn(saueTallinn);
+            setTallinnSaue(tallinnSaue);
+        };
+
+        dataFetch();
+    }, []);
+
+    return {saueTallinn, tallinnSaue};
+};
+
+const App = () => {
+
+    const { saueTallinn, tallinnSaue } = TrainTimeGetter()
+
+    const Train = (props: { label: string }) => {
+        if (!saueTallinn || !tallinnSaue) return (
             <div className={"mt-20 text-center"}>
-                <p>hi</p>
+                <TypeAnimation
+                    className="text-5xl font-bold text-emerald-500 text-center pt-5"
+                    sequence={[
+                        'patience',
+                        1000,
+                    ]}
+                    speed={50}
+                    cursor={false}
+                    repeat={0}
+                />
+                <TypeAnimation
+                    className="text-5xl font-bold text-emerald-500 text-center pt-5"
+                    sequence={[
+                        '', 1000, '.', 1000, '..', 1000, '...', 1000, '....', 1000, '.....', 1000, '......', 1000, '.......', 1000,
+                    ]}
+                    cursor={false}
+                    repeat={Infinity}
+                />
             </div>
         );
+        else {
+            return (
+                <div className={"mt-20 text-center"}>
+                    <h1 className="text-5xl font-bold text-emerald-500 text-center pt-5">{props.label}</h1>
+                </div>
+            );
+        };
     }
 
     return (
         <main className={"w-screen sm:h-screen bg-neutral-900"}>
             <title>Time...</title>
-            <h1 className="text-5xl font-bold text-emerald-500 text-center pt-5">Time | Time | Time</h1>
-            <div className={"justify-evenly flex text-center"}>
+            <h1 className="text-5xl font-bold text-emerald-500 text-center pt-5">Just a modicum of time...</h1>
+            <div className={"justify-evenly sm:flex text-center"}>
                 <div>
-                    <h1 className="text-5xl font-bold text-emerald-500 text-center pt-5">Saue → Tallinn</h1>
-                    <Train />
+                    <Train label={"Saue → Tallinn"} />
                 </div>
                 <div>
-                    <h1 className="text-5xl font-bold text-emerald-500 text-center pt-5">Tallinn → Saue</h1>
-                    <p className={"text-slate-100 mt-10 rounded w-32 sm:w-96 text-center leading-6 text-2xl bg-emerald-500 space-x-4 p-4 sm:p-10"}>test</p>
+                    <Train label={"Tallinn → Saue"} />
                 </div>
             </div>
         </main>
     )
 }
 
-export default ElronPage
+export default App
